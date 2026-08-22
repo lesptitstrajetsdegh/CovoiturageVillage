6aa6aa298f684e2778c54a7a7bc881ea16e19b01
Céline Degrave <lesptitstrajetsdegh@gmail.com>
2026-08-22 14:51:02 +0200

    Add Cloudflare Pages Function and Wrangler config

---
+++ functions/api/hello.js
@@ -0,0 +1,7 @@
+﻿export function onRequest() {
+  return new Response("Les P'tits trajets de Grand-Hallet - API OK", {
+    headers: {
+      "Content-Type": "text/plain; charset=UTF-8",
+    },
+  });
+}
